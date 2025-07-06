7bf8b112c0c6d7b81a5406a8a368272ad290206b
Akram Tarek <akramtarek231@gmail.com>
2025-07-07 02:20:11 +0300

    Set Difference problem solution

---
+++ Set_Difference.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+N = int(input())
+num1 = set(map(int, input().split()))
+M = int(input())
+num2 = set(map(int, input().split()))
+print(len(num1-num2))
